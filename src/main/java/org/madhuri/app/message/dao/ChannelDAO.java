113fad1333abe2b0ce5747dd7cad73a8f4202f50
Madhuri Github <madhuri.uttarkar@gmail.com>
2023-08-08 15:54:16 -0700

    made changes based on the comments

--- src/main/java/org/madhuri/app/message/dao/ChannelDAO.java
+++ src/main/java/org/madhuri/app/message/dao/ChannelDAO.java
@@ -4,26 +4,26 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
-import org.madhuri.app.message.model.MessageGroup;
+import org.madhuri.app.message.model.Channel;
 import org.madhuri.app.message.util.HibernateUtil;
 
-public class GroupDAO {
+public class ChannelDAO {
 
-	public List<MessageGroup> getgroups() {
+	public List<Channel> getchannels() {
 		Session session = HibernateUtil.getSession();
-		Query query = session.createQuery("from MessageGroup");
-		List<MessageGroup> groupmessages = query.list();
+		Query query = session.createQuery("from Channel");
+		List<Channel> channelmessages = query.list();
 		session.close();
-		return groupmessages;
+		return channelmessages;
 	}
 
-	public MessageGroup createGroup(MessageGroup group) {
+	public Channel createChannel(Channel channel) {
 		Session session = HibernateUtil.getSession();
 		Transaction transaction = null;
 
 		try {
 			transaction = session.beginTransaction();
-			session.save(group); // Save the new group entity
+			session.save(channel); // Save the new group entity
 			transaction.commit();
 		} catch (Exception e) {
 			if (transaction != null) {
@@ -34,18 +34,18 @@ public class GroupDAO {
 			session.close();
 		}
 
-		return group;
+		return channel;
 	}
 
-	public void deleteGroup(long groupid) {
+	public void deleteChannel(long id) {
 		Session session = HibernateUtil.getSession();
 		Transaction transaction = null;
 
 		try {
 			transaction = session.beginTransaction();
-			MessageGroup group = session.get(MessageGroup.class, groupid);
-			if (group != null) {
-				session.delete(group); // Delete the group entity
+			Channel channel = session.get(Channel.class, id);
+			if (channel != null) {
+				session.delete(channel); // Delete the group entity
 				transaction.commit();
 			}
 		} catch (Exception e) {
---
+++ src/main/java/org/madhuri/app/message/model/Channel.java
@@ -0,0 +1,123 @@
+package org.madhuri.app.message.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "channel")
+public class Channel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "admin_id")
+	private Long admin_id;
+
+	@Column(name = "welcome_message")
+	private String welcome_message;
+
+	@Column(name = "created_at")
+	private Date created_at;
+	
+	@Column(name="created_by")
+	private String created_by;
+	
+	@Column(name="updated_at")
+	private Date updated_at;
+	
+	@Column(name="updated_by")
+	private String updated_by;
+
+	public Channel() {
+
+	}
+
+	public Channel(Long id, String name, Long admin_id, String welcome_message, Date created_at, String created_by,
+			Date updated_at, String updated_by) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.admin_id = admin_id;
+		this.welcome_message = welcome_message;
+		this.created_at = created_at;
+		this.created_by = created_by;
+		this.updated_at = updated_at;
+		this.updated_by = updated_by;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Long getAdmin_id() {
+		return admin_id;
+	}
+
+	public String getWelcome_message() {
+		return welcome_message;
+	}
+
+	public Date getCreated_at() {
+		return created_at;
+	}
+
+	public String getCreated_by() {
+		return created_by;
+	}
+
+	public Date getUpdated_at() {
+		return updated_at;
+	}
+
+	public String getUpdated_by() {
+		return updated_by;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAdmin_id(Long admin_id) {
+		this.admin_id = admin_id;
+	}
+
+	public void setWelcome_message(String welcome_message) {
+		this.welcome_message = welcome_message;
+	}
+
+	public void setCreated_at(Date created_at) {
+		this.created_at = created_at;
+	}
+
+	public void setCreated_by(String created_by) {
+		this.created_by = created_by;
+	}
+
+	public void setUpdated_at(Date updated_at) {
+		this.updated_at = updated_at;
+	}
+
+	public void setUpdated_by(String updated_by) {
+		this.updated_by = updated_by;
+	}
+
+	
+}
